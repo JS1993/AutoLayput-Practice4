60d309927837e20d43f791533d3c1d07c2f902fd
susu <28025920@qq.com>
2016-04-18 19:35:13 +0800

    Child controls decide the height of the parent

---
+++ AutoLayout-Practice4/AutoLayout-Practice4Tests/AutoLayout_Practice4Tests.m
@@ -0,0 +1,39 @@
+//
+//  AutoLayout_Practice4Tests.m
+//  AutoLayout-Practice4Tests
+//
+//  Created by  江苏 on 16/4/18.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayout_Practice4Tests : XCTestCase
+
+@end
+
+@implementation AutoLayout_Practice4Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
